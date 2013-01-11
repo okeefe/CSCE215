2496b5a5101b2ea68720f739e57e7d2d2fe9d1b2
okeefe <okeefe@cse.sc.edu>
2013-01-11 12:07:45 -0500

    011113.1207

---
+++ name.h
@@ -0,0 +1,27 @@
+#define LAST	"Billy" 
+#define MI	" D."
+#define FIRST	"Grabasandwhich"
+
+class name {
+
+	private:
+	string first;
+	string middle;
+	string last;
+	
+	public:
+	void SetFirst(string str);
+	void GetFirst(string str);
+	
+	void SetMiddle(string str);
+	void GetMiddle(string str);
+	
+	void SetLast(string str);
+	void GetLast(string str);
+
+	void PrintLast();
+	void PrintMiddle();
+	void PrintFirst();
+	
+};
+
